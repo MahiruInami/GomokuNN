3159f0faba703aca1a4be92a7fd89a1747567207
Mahiru Inami <mahiruinamichan@gmail.com>
2023-08-07 19:41:24 +0300

    added imgui sources

---
+++ Sources/rlImGui-cs/simple_example/Program.cs
@@ -0,0 +1,48 @@
+﻿/*******************************************************************************************
+*
+*   raylib-extras [ImGui] example - Simple Integration
+*
+*	This is a simple ImGui Integration
+*	It is done using C++ but with C style code
+*	It can be done in C as well if you use the C ImGui wrapper
+*	https://github.com/cimgui/cimgui
+*
+*   Copyright (c) 2021 Jeffery Myers
+*
+********************************************************************************************/
+
+using System;
+
+using Raylib_cs;
+using rlImGui_cs;
+using ImGuiNET;
+
+namespace rlImGui_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.SetConfigFlags(ConfigFlags.FLAG_MSAA_4X_HINT | ConfigFlags.FLAG_VSYNC_HINT);
+            Raylib.InitWindow(1280, 800, "raylib-Extras-cs [ImGui] example - simple ImGui Demo");
+            Raylib.SetTargetFPS(144);
+
+            rlImGui.Setup(true);
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.DARKGRAY);
+
+                rlImGui.Begin();
+                ImGui.ShowDemoWindow();
+                rlImGui.End();
+
+                Raylib.EndDrawing();
+            }
+
+            rlImGui.Shutdown();
+            Raylib.CloseWindow();
+        }
+    }
+}
